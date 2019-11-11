b4737082d1805869dd406e10c2464f350c169dfd
yesicanfly <fgfdfszxz@163.com>
2019-11-11 08:56:35 +0000

    初始化样式

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,5 +1,16 @@
 
     @extends('layouts.default')
     @section('content')
-    <h1>主页</h1>
+    <div class="jumbotron">
+    <h1>Hello TQVAI华派</h1>
+    <p class="lead">
+    你现在所看到的是 <a href="https://tqvai.cn"></a>
+    </p>
+    <p>
+    一切，将从这里开始。
+    </p>
+    <p>
+    <a class="btn btn-lg btn-success" href="#" role="button">现在注册</a>
+    </p>
+    </div>
     @stop
