3cfccfaad4d36e5be52229b121577344a6235eef
yesicanfly <fgfdfszxz@163.com>
2019-11-14 03:15:01 +0000

    用户显示页面

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,12 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\User;
 class UsersController extends Controller
 {
     //
     public function create()
     {
         return view('users.create');
+    }
+
+    public function show(User $user)
+    {
+    return view('users.show', compact('user')); //将用户数据与视图进行绑定
     }
 }
