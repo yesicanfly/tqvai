cdf971df8940824eaf2e1c42d765d43d12947a60
yesicanfly <fgfdfszxz@163.com>
2019-11-15 05:50:44 +0000

    更改用户资料

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,7 @@ Route::get('/help','StaticPagesController@help')->name('help');
 Route::get('/about','StaticPagesController@about')->name('about');
 
 Route::get('/signup','UsersController@create')->name('signup');
+Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
 Route::resource('users', 'UsersController');
 
 Route::get('login', 'SessionsController@create')->name('login');
